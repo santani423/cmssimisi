f874da3a7b30c62915471195dab64ce1644fa372
santani423 <santani423@gmail.com>
2025-08-24 01:03:58 +0700

    perikan fiture

--- resources/views/cms/layout/index.blade.php
+++ resources/views/cms/layout/index.blade.php
@@ -105,11 +105,11 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
 
-                    <li class="nav-item">
+                    {{-- <li class="nav-item">
                         <form action="{{ route('profile') }}" method="GET" class="d-inline">
                             <button type="submit" class="nav-link btn btn-link">Profile</button>
                         </form>
-                    </li>
+                    </li> --}}
 
 
 
